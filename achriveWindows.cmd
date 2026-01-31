e602e9eb9b0a1f95810030bcda2abbeb657177cd
kechuan <2933885263@qq.com>
2026-01-31 20:13:21 +0800

    license add
    	new file:   LuckyBeastCardTemplateGenerator-0.2.0-windows-x64.rar
    
    	new file:   LuckyBeastCardTemplateGenerator-0.2.0-windows-x64.rar

--- achriveWindows.cmd
+++ achriveWindows.cmd
@@ -6,8 +6,8 @@ for /F "skip=3 tokens=2 delims=: " %%i in (pubspec.yaml) do (
 
 set VERSION=%VERSION:"=%
 
-echo BanguLite-%VERSION%
+echo LuckyBeastCardTemplateGenerator-%VERSION%
 
 set WINDOWS_BUILD_DIR=.\build\windows\x64\runner\Release\*
 
-rar a -r -ep1 banguLite-%VERSION%-windows-x64.rar %WINDOWS_BUILD_DIR%
+rar a -r -ep1 LuckyBeastCardTemplateGenerator-%VERSION%-windows-x64.rar %WINDOWS_BUILD_DIR%
